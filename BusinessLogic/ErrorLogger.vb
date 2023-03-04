fda216339192166f3c5fd5a5e1851959c4606f09
mortech69 <96631947+mortech69@users.noreply.github.com>
2023-03-04 10:56:08 -0500

    Error logger comments

--- BusinessLogic/ErrorLogger.vb
+++ BusinessLogic/ErrorLogger.vb
@@ -1,13 +1,21 @@
-﻿Public Class ErrorLogger
+﻿''' <summary>
+''' Logs Errors
+''' </summary>
+Public Class ErrorLogger
 
     Private ErrorSource = Nothing
-
+    ''' <summary>
+    ''' Constructor capture the source of error
+    ''' </summary>
+    ''' <param name="obj"></param>
     Public Sub New(obj As Object)
 
         ErrorSource = obj
 
     End Sub
-
+    ''' <summary>
+    ''' Method to log the error
+    ''' </summary>
     Public Sub LogError()
 
     End Sub
